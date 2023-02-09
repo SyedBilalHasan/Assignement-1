85ae3970f59ba453fd9b6f8e0a62d51be57413f6
SyedBilalHasan <123634861+SyedBilalHasan@users.noreply.github.com>
2023-02-09 20:49:41 +0500

    javascript class 3

---
+++ class3/app.js
@@ -0,0 +1,161 @@
+  // chapter5
+  
+  //Question 1
+  
+//   var num1 = 3
+//   var num2 = 5
+//   var total=(num1 + num2)
+
+//  document.write("sum of 3 and 5 is" +" "+ total)
+
+
+  //Question 2
+
+
+//   var num1 =5
+//   var num2 =3
+//   var total= (num1 - num2 )
+
+// document.write("subtraction of 5 and 3 is"+" "+total)
+
+
+// var num1 =5
+// var num2 =3
+// var total= (num1 * num2 )
+
+// document.write("multiplication of 5 and 3 is"+" "+total)
+
+
+// var num1 =5
+// var num2 =3
+// var total= (num1 / num2 )
+
+// document.write("division of 5 and 3 is"+" "+total)
+
+
+
+// var num1 =5
+// var num2 =3
+// var total= (num1 % num2 )
+
+// document.write("modulus of 5 and 3 is"+" "+total)
+
+//Question 4
+
+
+// var ticket = 5
+// var price = 600
+
+// var total= (ticket*price)
+
+// document.write("Total cost to buy 5 tickets to a movies is"+" "+total + "  "  +"PKR")
+
+
+
+//Question 5
+// var tabel = 4
+// document.write("Tabel of 4"+"<br />")
+
+// document.write("4 x 1 = "+tabel*1  + "<br />" + "4 x 2 = "+tabel*2  + "<br />" + "4 x 3 = "+tabel*3   + "<br />" + "4 x 4 = "+tabel*4     
+// + "<br />" + "4 x 5 = "+tabel*5      + "<br />" + "4 x 6 = "+tabel*6   + "<br />" + "4 x 7 = "+tabel*7    + "<br />" + "4 x 8 = "+tabel*8
+// + "<br />" + "4 x 9 = "+tabel*9      + "<br />" + "4 x 10 = "+tabel*10)
+
+
+//Question 6
+
+
+// var item = 650
+// var Quantity = 3
+
+// var item1= 100
+
+// var Quantity1 = 7
+
+// var shippingCharge=100
+
+
+// document.write("Total Cost of your order is "+ (item*Quantity + item1*Quantity1 + shippingCharge ))
+
+
+
+
+//Question 7
+
+// var total = 980
+
+// var mark = 804
+
+// var total = (mark / total *100)
+
+// document.write("Marks sheet " +"<br />" + "Total Marks: 980"+"<br />" +  "Marks obtained :804" +"<br />" + "percentage:"+ total )  
+
+
+//Question 9
+
+// var us = 10
+// var rate =104.80
+// var riyal =25
+// var rate1=28
+
+// var total=(us* rate)
+
+// var total1 = (riyal * rate1)
+
+// console.log(total1)
+
+
+// document.write("Currency in PKR" +"<br />" +  "total currency in PKR:" +(total+total1))
+
+
+
+//Question 11
+
+// var  currentYear= 2023
+
+// var birthYear = 2002
+
+// var total = (currentYear-birthYear)
+
+// // console.log(total)
+
+// document.write("Current Year" +" "+total)
+
+
+
+
+  // chapter 6-9
+
+// Question 1
+
+
+// document.write("Result" + "<br />"+ "The value of a is :10" + "<br />")
+
+
+// document.write("The value of ++a is :11" + "<br />"+ "The value of a is :11" + "<br />")
+
+// document.write("The value of a++ is :11" + "<br />"+ "The value of a is :12" + "<br />")
+
+
+
+// document.write("The value of --a is :11" + "<br />"+ "The value of a is :11" + "<br />")
+
+
+
+// document.write("The value of a-- is :11" + "<br />"+ "The value of a is :10" + "<br />")
+
+
+
+// Question 2
+
+
+
+var a = 2, b = 1;
+var result = --a- --b + ++b + b--;
+
+console.log(result)
+
+
+--a; 1
+--a- --b; 1
+--a - --b + ++b;2
+--a- --b + ++b + b--;3
