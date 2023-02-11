100b4a5002a05ab10b5bb2f59557d09b80674573
SyedBilalHasan <123634861+SyedBilalHasan@users.noreply.github.com>
2023-02-11 16:39:38 +0500

    class 4

---
+++ class4/app.js
@@ -0,0 +1,122 @@
+
+//Chapter9-10
+
+
+//Question1
+
+// var city1="karachi"
+
+
+// var city= prompt("Enter your City")
+
+// if(city==city1){
+    
+//         alert("Welcome to city of lights")
+       
+// } else{
+//     alert("not found")
+// }
+
+
+
+//Question 2
+
+// var m ="male"
+// var f ="female"
+
+// var gender=prompt("Enter your Gender")
+
+
+// if (gender==m){
+//     alert("Good Morning Sir.") 
+// }
+
+// if(gender==f){
+//     alert("Good Morning Ma’am.")
+//     }
+
+// else{
+//     alert("not found")
+
+// }
+
+//Question3
+
+// var r ="Red"
+// var y="yellow"
+// var G="Green"
+
+// var colour=prompt("single Color")
+
+
+// if(colour==r){
+//     alert("Must Stop")
+// }
+// if(colour==y){
+//     alert("Ready to move")
+// }
+// if(colour==G){
+//     alert("Move now")
+// }
+
+
+//Question4
+
+// var fule="0.25 liters"
+
+// var liters=prompt("input current fule")
+
+// if(liters==fule){
+//     alert("Please refill the fuel in your car" )
+// }
+
+//Question 5
+
+// var num =6
+// var num1=prompt("Enter the secret Number")
+
+// if (num1==num){
+
+//     alert("Bingo! Correct answer")
+// }else{
+//     alert("Close enough to the correct answer")
+// }
+
+
+
+//Question 10
+
+// var t1 =40
+// var t2=30
+// var t3=20
+// var t4=10
+
+// var temp=prompt("Enter your Temperature")
+// if (temp==t1){
+//     alert("It is too hot outside")
+// }
+
+// if (temp==t2){
+//     alert("The Weather today is Normal")
+// }
+// if (temp==t3){
+//     alert("Today’s Weather is cool")
+// }
+// if (temp==t4){
+//     alert("OMG! Today’s weather is so Cool")
+// }
+
+//Chapter 12-13
+
+//Question 6
+
+// var hour = 13;
+
+
+// var work=prompt("Enter hours")
+
+// if(hour < 18){
+//     alert("Good day")
+// }else{
+//     alert("Good Evening")
+// }
