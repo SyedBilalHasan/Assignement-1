8f71569d1036f7818b39907d53a23d828d0b85dc
SyedBilalHasan <123634861+SyedBilalHasan@users.noreply.github.com>
2023-02-18 19:59:43 +0500

    Assignment5

---
+++ class5a/app.js
@@ -0,0 +1,119 @@
+// chapter
+
+
+// Question 1
+
+// var nun1=["      "]
+// console.log(nun1)
+
+
+// Question 2
+
+
+// var num1=["bilal","hasan"]
+
+
+// Question 3
+
+// var num1=["syed","bilal",'hasan']
+
+
+// Question 4
+
+// var num1=[1,2,3,4,5,6,7,8,9]
+
+
+// Question 5
+
+// var num1=['true','false']
+// console.log(num1)
+
+
+//Question 6
+// var num1=['true',1,'bilal']
+
+//Question 7
+
+
+// var edu=['SSC', 'HSC', 'BCSS' ,'BS', 'BCOM', 'MS', 'Phil.', 'PhD']
+// console.log(edu)
+
+
+// document.write("1)"+edu[0] +"<br />")
+// document.write("2)"+edu[1]+"<br />")
+// document.write("3)"+edu[2]+"<br />")
+// document.write("4)"+edu[3]+"<br />")
+// document.write("5)"+edu[4]+"<br />")
+// document.write("6)"+edu[5]+"<br />")
+// document.write("7)"+edu[6])
+
+
+
+//Question 6
+
+// var std=["bilal","dilawar","hasan"]
+
+// var sco=[355,468,358]
+
+// document.write(std[0]+"  "+ sco["0"]+"    "  +"Percentage:"+(100*355 )/ 500 +"<br />")
+// document.write(std[1]+"  "+ sco["1"]+"    "  +"Percentage:"+(100*468 )/ 500 +"<br />")
+// document.write(std[2]+"  "+ sco["2"]+"    "  +"Percentage:"+(100*358 )/ 500)
+
+
+
+//Question 10
+
+// var num1=[320,230,480,120]
+// num1.sort()
+// console.log(num1)
+
+
+//Question 12
+
+
+// var arr =["This", " is ", " my ", " cat"];
+// var a= arr.join("")
+// console.log(a)
+
+
+// Chapter 17-20
+
+
+//Question 1
+
+
+// var arr=["bilal",("bilal")]
+
+
+
+//Question 2
+
+// var arr1=[0,1,2,3]
+// var arr2=[1,0,1,2]
+// var arr3=[2,1,0,1]
+//  console.log(arr1,arr2,arr3)
+
+
+//Question 3
+// var arr1=[0,1,2,3,4,5,6,8,9]
+
+
+
+
+//Question 7
+var a=["cake","apple pie","cookie","chips","patties"]
+var a=prompt("Enter your food")
+
+
+if(a=="cake"){
+    alert("cake is Available");
+
+    if(a=="apple pie"){
+        alert("apple pie is Avalibal")
+    };
+
+
+    
+}else{
+    alert("not Avalibal")
+}
