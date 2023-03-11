0b97ef2223784cf696c3966dabed69a748aeaec8
SyedBilalHasan <123634861+SyedBilalHasan@users.noreply.github.com>
2023-03-11 08:29:00 +0500

    Assignment 6

---
+++ class6/app.js
@@ -0,0 +1,73 @@
+// Chapter 21 - 25
+
+// Question no 2
+
+// var month = prompt("Enter a month");
+// var charsInMonth = month.length;
+// document.write(charsInMonth)
+
+
+// Question no 3
+
+// var num1 = "pakistani"
+// document.write(num1.indexOf("n"))
+
+// Question no 4
+
+
+// var num1 = "Hello World"
+// document.write(num1.lastIndexOf("l"))
+
+
+// Question no 5
+
+// var num1 = "Pakistani"
+// document.write(num1.charAt(3))
+
+
+// Question no 7
+
+
+// var city = "Islam"
+// for (var i = 0; i < city.length; i++) {
+//     if (city.slice(i, i + 5) === "Islam") {
+//         city = city.slice(0, i) + city.slice(i) + "abad"
+//         break;
+//     }
+
+// }
+// console.log(city)
+
+
+
+// Question no 8
+
+// var para = "Ali and Sami are best friends. They play cricket and football together"
+// console.log(para.replaceAll("and", "&"))
+
+
+// Question no 9
+
+
+
+// console.log(parseInt("472"))
+
+
+
+// Question no 10
+
+// var num1 = 'peanuts'
+// console.log(num1.toUpperCase())
+
+
+
+// Question no 11
+
+
+// var name1 = prompt("Enter The Name")
+
+// document.write(name1.toUpperCase())
+
+
+
+
