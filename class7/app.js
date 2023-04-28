e62363559e6efd52c9a74d46b07733fef2f71e89
SyedBilalHasan <123634861+SyedBilalHasan@users.noreply.github.com>
2023-04-28 21:59:21 +0500

    Assignment 7

---
+++ class7/app.js
@@ -0,0 +1,76 @@
+// Assignment 7
+
+// Question 1
+
+// var num = 3.45214
+// num = Math.random()
+// document.write(num)
+// console.log(num)
+
+// var num = 3.45214
+//  num = Math.round(num)
+// document.write(num)
+
+
+
+// var num = 3.45214
+//  num = Math.floor(1.66666)
+// document.write(num)
+
+
+// var num = 3.45214
+//  num = Math.ceil(1.22222)
+// document.write(num)
+
+
+// Question 2
+
+
+// var num = -2.673
+//  num = Math.round(num)
+// document.write(num)
+
+
+
+// var num = -2.673
+//  num = Math.floor(num)
+// document.write(num)
+
+
+// var num = -2.673
+//  num = Math.ceil(num)
+// document.write(num)
+
+
+
+
+// Question 4
+
+
+// var roll = Math.floor(Math.random() * 6) + 1;
+
+// console.log(roll)
+
+
+
+// Question 5
+
+
+// var toss = Math.random();
+
+// var result;
+// if (toss < 0.5) {
+//   result = "Heads";
+// } else {
+//   result = "Tails";
+// }
+
+// console.log(result)
+
+
+// Question 6
+
+
+// var number = Math.floor(Math.random() * 100) + 1;
+// console.log(number)
+
